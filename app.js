3dd505345a2c90b848c6fbeaee7fed7fcc8c5ffb
RaczekNieBoRaczek <wertyksonred@gmail.com>
2022-08-21 01:59:22 +0200

    Better but harder concept on canvas

--- app.js
+++ app.js
@@ -1,2 +1,68 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 
-  
+const egg = new Image();
+egg.src = 'images/egg.png';
+
+const animals = new Image();
+animals.src = 'images/animals.png';
+
+function roundRect(x, y, w, h, radius)
+{
+  var r = x + w;
+  var b = y + h;
+  ctx.beginPath();
+  ctx.moveTo(x+radius, y);
+  ctx.lineTo(r-radius, y);
+  ctx.quadraticCurveTo(r, y, r, y+radius);
+  ctx.lineTo(r, y+h-radius);
+  ctx.quadraticCurveTo(r, b, r-radius, b);
+  ctx.lineTo(x+radius, b);
+  ctx.quadraticCurveTo(x, b, x, b-radius);
+  ctx.lineTo(x, y+radius);
+  ctx.quadraticCurveTo(x, y, x+radius, y);
+  ctx.stroke();
+}
+
+class myBtn{
+    constructor(x, y, w, h, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.radius = radius;
+        this.color = color;
+    }
+    
+     drawObject() {
+        ctx.roundRect(this.x, this.y, this.w, this.h, this.radius);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+
+
+function scalePreserveAspectRatio(imgW,imgH,maxW,maxH){
+    return(Math.min((maxW/imgW),(maxH/imgH)));
+}
+
+function drawChoseMenu(){
+    var w=egg.width;
+    var h=egg.height;
+    var sizer=scalePreserveAspectRatio(w,h,canvas.width,canvas.height);
+    ctx.drawImage(egg,0,0,w,h,canvas.width/4,canvas.height/8,w*sizer/2,h*sizer/2);   
+    ctx.drawImage(animals,64,149,15,10,canvas.width/4,canvas.height/8,150,100);   
+
+};
+
+window.onload = function() {
+    drawChoseMenu();
+}
+
+
+
+
+
